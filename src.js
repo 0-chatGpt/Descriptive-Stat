273d330b7c291fa0a3a935474c4f758bbe2c536d
chinemeremun okereke <chinemeremun.okereke.236535@unn.ng>
2024-01-28 14:04:32 +0100

    Fixed major bugs

--- src.js
+++ src.js
@@ -1,7 +1,7 @@
 "use strict";
 
 class Stat {
-  constructor(dataset = [], interval = null, freq = null, grouped = false) {
+  constructor(dataset = [], interval = [], freq = [], grouped = false) {
     /***
      * dataset = [1,2,3,4,5,2,23,4,6,...]
      * interval = [[10,15], [20,25], [30,35], ...]
@@ -12,12 +12,12 @@ class Stat {
     this.grouped = grouped;
     this.freq = freq;
     this.raw = [...dataset];
-    this.sum = dataset.reduce((acc, _x, i) => {
-      acc = acc + _x;
-    }, 0);
     this.midpoints = interval.map((val, i) => (val[0] + val[1]) / 2);
     this.dataset = dataset;
     this.dataset.sort((a, b) => a - b);
+    this.sum = this.dataset.reduce((acc, _x, i) => {
+      return (acc = acc + _x);
+    }, 0);
   }
 
   cumFreq() {
@@ -35,7 +35,9 @@ class Stat {
       return this.sum / this.dataset.length;
     } else {
       let sum_f_i = this.freq.reduce((acc, _x) => (acc += _x), 0);
-      let sum_m_ixf_i = this.freq.map((val, i) => val * this.midpoints[i]);
+      let sum_m_ixf_i = this.freq
+        .map((val, i) => val * this.midpoints[i])
+        .reduce((acc, val) => (acc += val), 0);
       return sum_m_ixf_i / sum_f_i;
     }
   }
@@ -46,8 +48,8 @@ class Stat {
         return this.dataset[(this.dataset.length + 1) / 2];
       } else {
         return (
-          (this.dataset[this.dataset.length / 2] +
-            this.dataset[this.dataset.length / 2 + 1]) /
+          (this.dataset[Math.floor(this.dataset.length / 2)] +
+            this.dataset[Math.floor(this.dataset.length / 2) - 1]) /
           2
         );
       }
@@ -79,7 +81,7 @@ class Stat {
       });
       let mode = [];
       let maxCount = 0;
-      for (val in count) {
+      for (let val in count) {
         if (count[val] > maxCount) {
           mode = [val];
           maxCount = count[val];
@@ -89,19 +91,20 @@ class Stat {
         return mode;
       }
     } else {
-      let max = Math.max(this.freq);
-      let modes = freq
+      let max = Math.max(...this.freq);
+      let modes = this.freq
         .map((val, i) => [val, i])
         .filter((val) => val[0] === max);
       let mode_interval = [];
       modes.forEach((val) => mode_interval.push(this.interval[val[1]]));
-      return mode_interval.map((val) => val[1] - val[0]);
+      return mode_interval;
+      //   return mode_interval.map((val) => (val[1] + val[0]/2));
     }
   }
 
   range() {
     if (!this.grouped) {
-      return Math.max(this.dataset) - Math.min(this.dataset);
+      return Math.max(...this.dataset) - Math.min(...this.dataset);
     } else {
       let H = this.interval[this.interval.length - 1][1];
       let L = this.interval[0][0];
@@ -137,7 +140,7 @@ class Stat {
 
   standardDeviation() {
     let variance = this.variance();
-    return variance ** 1 / 2;
+    return variance ** 0.5;
   }
 
   meanDeviation() {
@@ -152,7 +155,7 @@ class Stat {
     } else {
       let sum_f_i = this.freq.reduce((acc, val) => (acc += val), 0);
       let diff = this.interval.map((val, i) => {
-        return Math.abs(val - mean);
+        return Math.abs((val[0] + val[1]) / 2 - mean);
       });
       return (
         (1 / sum_f_i) *
@@ -166,12 +169,12 @@ class Stat {
   quartileDevaition() {
     if (!this.grouped) {
       return (
-        (this.dataset[(this.dataset.length + 1) / 4] -
-          this.dataset[(3 * (this.dataset.length + 1)) / 4]) /
+        (this.dataset[Math.floor((3 * (this.dataset.length + 1)) / 4)] -
+          this.dataset[Math.floor((this.dataset.length + 1) / 4)]) /
         2
       );
     } else {
-      let cumArr = this.cumFreq;
+      let cumArr = this.cumFreq();
       let cum_freq_1 = [0, 0];
       let cum_freq_2 = [0, 0]; // [cummulative , index]
       let sum_f_i = this.freq.reduce((acc, val) => (acc += val), 0);
@@ -182,26 +185,52 @@ class Stat {
             cum_freq_1[1] = i;
           }
         }
+
         if (!cum_freq_2[1]) {
           cum_freq_2[0] += val;
           if (cum_freq_2[0] >= (3 * sum_f_i) / 4) {
             cum_freq_2[1] = i;
           }
         }
-        return (
+      });
+      //   console.log(cum_freq_1, cum_freq_2);
+      return (
+        (this.interval[cum_freq_2[1]][0] +
+          (((3 * sum_f_i) / 4 - cumArr[cum_freq_2[1] - 1]) *
+            (this.interval[cum_freq_2[1]][1] -
+              this.interval[cum_freq_2[1]][0])) /
+            this.freq[cum_freq_2[1]] -
           (this.interval[cum_freq_1[1]][0] +
             ((sum_f_i / 4 - cumArr[cum_freq_1[1] - 1]) *
               (this.interval[cum_freq_1[1]][1] -
                 this.interval[cum_freq_1[1]][0])) /
-              this.freq[cum_freq_1[1]] -
-            (this.interval[cum_freq_2[1]][0] +
-              (((3 * sum_f_i) / 4 - cumArr[cum_freq_2[1] - 1]) *
-                (this.interval[cum_freq_2[1]][1] -
-                  this.interval[cum_freq_2[1]][0])) /
-                this.freq[cum_freq_2[1]])) /
-          2
-        );
-      });
+              this.freq[cum_freq_1[1]])) /
+        2
+      );
+      //   });
     }
   }
 }
+
+// // let df = new Stat([25, 30, 35, 40, 45, 50, 55, 60, 65, 70]);
+// let df = new Stat(
+//   [],
+//   [
+//     [20, 29],
+//     [30, 39],
+//     [40, 49],
+//     [50, 59],
+//     [60, 69],
+//   ],
+//   [3, 5, 8, 6, 4],
+//   true
+// );
+// // console.log(df);
+// console.log(df.mean());
+// console.log(df.median());
+// console.log(df.mode()); //error
+// console.log(df.range());
+// console.log(df.quartileDevaition());
+// console.log(df.meanDeviation());
+// console.log(df.standardDeviation());
+// console.log(df.variance());
